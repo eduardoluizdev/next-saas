e7f2a808185f3147ac764b9d39f2f20f8a169c0a
Eduardo Porciuncula <hello@eduardoluiz.dev>
2025-03-08 05:40:40 -0300

    feat: add permissions initial setup

--- configs/prettier/index.mjs
+++ configs/prettier/index.mjs
@@ -2,23 +2,21 @@
 
 /** @type {PrettierConfig} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
-  tailwindConfig: "./tailwind.config.js",
-  tailwindFunctions: ["clsx"],
   tabWidth: 2,
   useTabs: false,
   semi: false,
   singleQuote: true,
-  quoteProps: "as-needed",
+  quoteProps: 'as-needed',
   jsxSingleQuote: false,
-  trailingComma: "es5",
+  trailingComma: 'es5',
   bracketSpacing: true,
   jsxBracketSameLine: false,
-  arrowParens: "always",
-  endOfLine: "auto",
-  embeddedLanguageFormatting: "auto",
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  embeddedLanguageFormatting: 'auto',
   singleAttributePerLine: true,
   bracketSameLine: false,
-};
+  printWidth: 120
+}
 
-export default config;
+export default config
